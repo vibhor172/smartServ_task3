b91637162f26d54b2a88caee5835644f87623b58
Vibhor <vibhorpandey.2013@gmail.com>
2024-01-02 12:20:41 +0530

    Frustrated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Login from './Login';
 import './App.css';
 import Dashboard from './Dashboard';
-import { Routes, Route  ,BrowserRouter } from 'react-router-dom';
+import { Routes, Route  , HashRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
+       < HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
-      </BrowserRouter>
+      </ HashRouter>
     </div>
    
   );
